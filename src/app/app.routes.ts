edf7595fc12cf2d4bc93638904c89e2ddd2c1ae7
Joris Bürgin <joris.buergin@swisscom.com>
2026-06-05 13:21:07 +0200

    feat: Routing & Navigation implementieren

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './pages/login-page/login-page';
+import { NoAccessPage } from './pages/no-access-page/no-access-page';
+import { ChallengeListPage } from './pages/challenge-list-page/challenge-list-page';
+import { ChallengeFormPage } from './pages/challenge-form-page/challenge-form-page';
+import { CategoryListPage } from './pages/category-list-page/category-list-page';
+import { CategoryFormPage } from './pages/category-form-page/category-form-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', redirectTo: 'challenges', pathMatch: 'full' },
+	{ path: 'login', component: LoginPage },
+	{ path: 'no-access', component: NoAccessPage },
+
+	{ path: 'challenges', component: ChallengeListPage },
+	{ path: 'challenges/new', component: ChallengeFormPage },
+	{
+		path: 'challenges/:id',
+		loadComponent: () => import('./pages/challenge-detail-page/challenge-detail-page').then(m => m.ChallengeDetailPage)
+	},
+	{ path: 'challenges/:id/edit', component: ChallengeFormPage },
+
+	{ path: 'categories', component: CategoryListPage },
+	{ path: 'categories/new', component: CategoryFormPage },
+	{ path: 'categories/:id/edit', component: CategoryFormPage },
+
+	{ path: '**', redirectTo: 'challenges' }
+];
--- src/app/components/nav-bar/nav-bar.ts
+++ src/app/components/nav-bar/nav-bar.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [CommonModule, MatToolbarModule, MatButtonModule],
+  imports: [CommonModule, RouterModule, MatToolbarModule, MatButtonModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
